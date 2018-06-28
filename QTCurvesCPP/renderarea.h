fbbda14fd7cdaffc2de2797613bb8ed166697eca
Unknown <andrestc94@hotmail.com>
2018-06-28 15:32:20 -0500

    Project 1 Update

---
+++ QTCurvesCPP/renderarea.h
@@ -0,0 +1,29 @@
+#ifndef RENDERAREA_H
+#define RENDERAREA_H
+
+#include <QWidget>
+
+class RenderArea : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit RenderArea(QWidget *parent = nullptr);
+
+    QSize minimumSizeHint() const Q_DECL_OVERRIDE;
+    QSize sizeHint() const Q_DECL_OVERRIDE;
+
+    enum Shapes {Asteroid,Cicloid,HuygenCicloid,HypoCicloid};
+
+protected:
+    void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
+
+signals:
+
+public slots:
+
+private:
+    QColor mBackgroundColor;
+    QColor mShapeColor;
+};
+
+#endif // RENDERAREA_H
